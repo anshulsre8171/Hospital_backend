a6ee83c8cf87b87d63164b8d1b90dc13fd61d638
anshulsre8171 <anshulsre8171@gmail.com>
2025-04-09 19:04:32 +0530

    File uploads lib and Registration Api

--- src/Contollers/CommonController/LoginFunctionController.ts
+++ src/Contollers/CommonController/LoginFunctionController.ts
@@ -1,5 +1,7 @@
 import { createResponse } from "../../Helpers/createResponse";
-
+import { returnUserType } from "../../Helpers/returnUserType";
+import path from "path";
+import { uploadFileHelper } from "../../Helpers/uploadFileHelper";
 
 
 
@@ -8,6 +10,28 @@ export const userRegisterController = async (req: any, res: any) => {
         const dataToSave = req.body;
         let { profile } = req.files;
         console.log(dataToSave);
+
+        const pathToSaveFile = path.join(__dirname, '../../uploads/');
+        const profileName = uploadFileHelper(profile, pathToSaveFile, res);
+        const finalData: any = { ...dataToSave, profile: profileName };
+        // console.log(finalData);
+        
+        const TblName: any = await returnUserType(dataToSave?.userType);
+        // console.log(TblName);
+        
+        const isExist = await TblName.findOne({ where: { email: dataToSave?.email } });
+        // console.log(isExist);
+        if (isExist) {
+            return createResponse(res, 208, "User Already Exist !", isExist, false, true)
+        } else {
+            const result = await TblName.save(finalData);
+            // console.log(result);
+            if (result){
+                return createResponse(res, 201, "User register successfully !", result, true, false)
+            }else{
+                return createResponse(res, 404, "User register failed !", result, false, true)
+            }
+        }
   
     } catch (err) {
         if (err) {
