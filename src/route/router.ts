657c6ee44dc1ebf5588e79765eba314eedb6c008
anshulsre8171 <anshulsre8171@gmail.com>
2025-04-12 17:20:50 +0530

    create middleware and verify jwtToken

--- src/route/router.ts
+++ src/route/router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {userRegisterController,userLoginController, ForgetPassswordController, ResetPasswordController, getuserPic,  } from '../Contollers/CommonController/LoginFunctionController';
 import { addDepartmentController } from '../Contollers/AdminController/DepartmentController';
+import { verifyToken } from '../middleware/Verifytoken';
 export const route=express.Router();
 
 //common route
@@ -13,7 +14,7 @@ route.get('/user/:id/:userType', getuserPic);
 
 
 //admin route
-route.post('/admin-add-department', addDepartmentController);
+route.post('/admin-add-department',verifyToken, addDepartmentController);
 
 //doctor route
 
